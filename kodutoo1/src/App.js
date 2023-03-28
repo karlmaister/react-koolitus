cd45ddf184f7d37d757966670099c28ef3d49aee
karlmaister <128396233+karlmaister@users.noreply.github.com>
2023-03-28 11:35:47 +0300

    kodutöö 8

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -4,15 +4,44 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Avaleht from './pages/Avaleht';
 import Kontakt from './pages/Kontakt';
 import Meist from './pages/Meist';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const kasutajaNimiRef = useRef();
+  const paroolRef = useRef();
+
+    const logiSisse = () => {
+
+      if (paroolRef.current.value === "123") {
+
+        muudaSisselogitud("jah")
+        muudaSonum(kasutajaNimiRef.current.value + ", oled sisselogitud")
+      } else {
+        muudaSonum("Vale parool")
+        }
+      }
+  
+
   return (
     
     
     <div>
-    
+      <div>{sonum}</div>
+
+      {sisselogitud === "ei" && <div>
+        <label>Kasutajanimi</label>
+        <input ref={kasutajaNimiRef} type="text"/>
+       <label>Parool</label>
+        <input ref={paroolRef}type="password"/>
+      </div>}
+
+    {sisselogitud==="ei" && <button onClick={logiSisse}>Logi sisse</button>} 
+    {sisselogitud==="jah" && <button onClick={() => muudaSisselogitud("ei")}>Logi välja</button>} <br />
+
     <Link to='/'>
-        <button>Avalehet</button>
+        <button>Avaleht</button>
       </Link>
       <Link to='/kontakt'>
         <button>Kontakt</button>
